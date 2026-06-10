1f087449e26b237af63e57d51c6149522d9150ec
Quan Lee <weiquanlee95@Quans-MacBook-Air.local>
2026-06-11 06:42:26 +0800

    Initial commit

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,28 @@
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int reminder(int i, int j){
+		return i%j;
+	}
+
+	// public void sayHello(){
+	// 	String name = null;
+	// 	name.toString();
+	// }
+	
+}
